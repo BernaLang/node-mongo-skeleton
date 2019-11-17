83bc3602995b09277d4d86971b6245cbf8aead05
Bernardo Amaral <bernardomouta13@gmail.com>
2019-11-17 18:01:14 +0000

    User model separation

--- server/models/index.js
+++ server/models/index.js
@@ -2,17 +2,9 @@
 * DB models.
 */
 'use strict';
-var mongoose = require('mongoose');
 
-var userSchema = new mongoose.Schema({
-	username: String,
-	name: { type: String, trim: true },
-	age: { type: Number, min: 0 },
-	gender: String
-})
-
-var UserModel = mongoose.model('users', userSchema);
+let { userModel } = require('./users');
 
 module.exports = {
-  UserModel
+  User: userModel
 };
--- server/models/users.js
+++ server/models/users.js
@@ -1,7 +1,15 @@
-/**
-* USERS model.
-*/
-'use strict';
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+	username: { type: String, index: { unique: true }, required: true, trim: true },
+	name: { type: String, trim: true },
+	age: { type: Number, min: 0 },
+  gender: { type: String, enum: ["male", "female"], lowercase: true },
+  _deleted: { type: Boolean, default: false }
+})
 
+let User = mongoose.model('users', userSchema);
 
+module.exports = {
+	userModel: User
+}
